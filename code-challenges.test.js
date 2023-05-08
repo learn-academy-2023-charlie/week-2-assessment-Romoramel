c8b703261be672c5e21b1cd76f40b939212f9682
Romoramel <romoramel@hotmail.com>
2023-05-08 08:32:37 -0700

    completed

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -1,5 +1,7 @@
 // ASSESSMENT 2: Coding Practical Questions with Jest
 
+const { captureRejectionSymbol } = require("events")
+
 // Please read all questions thoroughly
 // Pseudo coding is REQUIRED
 // If you get stuck, please leave comments to help us understand your thought process
@@ -14,12 +16,27 @@
 
 // Pseudo code:
 
-// a) Create a test with expect statements for each of the variables provided.
+// a) Create a test with expect statements for each of the variables provided. 
+
+    describe("multiplesOfThree", () => {
+        it("returns values mulptipled by 3", () => {
+            expect(multiplesOfThree([6, 7, 8, 9, 10])).toEqual([18, 21, 24, 27, 30])
+            expect(multiplesOfThree([24, 27, 30, 33, 36])).toEqual([72, 81, 90, 99, 108])
+        })
+    })
 
 const numbersArray1 = [6, 7, 8, 9, 10]
+
+    const multiplesOfThree = (Array) => {
+    return Array.map((number) => number * 3)
+}
+    console.log(multiplesOfThree(numbersArray1))
+
 // Expected output: [18, 21, 24, 27, 30]
 const numbersArray2 = [24, 27, 30, 33, 36]
 // Expected output: [72, 81, 90, 99, 108]
+
+console.log(multiplesOfThree(numbersArray2))
 
 // b) Create the function that makes the test pass.
 
@@ -28,6 +45,17 @@ const numbersArray2 = [24, 27, 30, 33, 36]
 // Pseudo code:
 
 // a) Create a test with expect statements for each of the variables provided.
+
+describe('DIVISIBLEBYTHREE', () => {
+    it('takes an object as an argument and decides is the number inside it is evenly divisdible by three or not', () => {
+        const object1 = { number: 15 };
+        const object2 = { number: 0 };
+        const object3 = { number: -7 };
+        expect(divisibleByThree(object1)).toEqual("15 is divisible by three")
+        expect(divisibleByThree(object2)).toEqual("0 is divisible by three")
+        expect(divisibleByThree(object3)).toEqual("-7 is divisible by three")
+    })
+})
 
 const object1 = { number: 15 }
 // Expected output: "15 is divisible by three"
@@ -38,11 +66,25 @@ const object3 = { number: -7 }
 
 // b) Create the function that makes the test pass.
 
+const divisibleByThree = (object) => {
+    if(object.number % 3 === 0) {
+        return '${object.number} is divisible by three'
+    }
+}
 // --------------------2) Create a function that takes in an array of words and returns an array with all the words capitalized.
 
 // Pseudo code:
 
 // a) Create a test with expect statements for each of the variables provided.
+
+describe('capArr', () => {
+    it('takes an array and makes all words capital', () => {
+        const randomNouns1 = ["streetlamp", "potato", "teeth", "conclusion", "nephew"]
+        expect(capArr(randomNouns1)).toEqual(["streetlamp", "potato", "teeth", "conclusion", "nephew"])
+        const randomNouns2 = ["temperature", "database", "chopsticks", "mango"];
+        expect(capArr(randomNouns2)).toEqual (["temperature", "database", "chopsticks", "mango"])
+     })
+})
 
 const randomNouns1 = ["streetlamp", "potato", "teeth", "conclusion", "nephew"]
 // Expected output: ["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"]
@@ -51,11 +93,24 @@ const randomNouns2 = ["temperature", "database", "chopsticks", "mango"]
 
 // b) Create the function that makes the test pass.
 
+const capArr = (array) => {
+    return array.map((value) => value.chart(0).toUppercase() + value.slice(1))
+}
+
 // --------------------3) Create a function that takes in a string and logs the index of the first vowel.
 
 // Pseudo code:
 
 // a) Create a test with expect statements for each of the variables provided.
+
+it('takes in a string and logs the index of the first vowel', () => {
+    const vowelTester1 = "learn"
+    const vowelTester2 = "academy"
+    const vowelTester3 = "challenges"
+    expect(first(vowelTester1)).toEqual(1)
+    expect(first(vowelTester2)).toEqual(0)
+    expect(first(vowelTester3)).toEqual(2)
+})
 
 const vowelTester1 = "learn"
 // Expected output: 1
@@ -65,3 +120,13 @@ const vowelTester3 = "challenges"
 // Expected output: 2
 
 // b) Create the function that makes the test pass.
+
+const firstV = (array) => {
+    const vowels = ['a', 'e', 'i', 'o', 'u']
+    for(let i = 0; i < array.length; i++) {
+        if(vowels.includes(array[i])) {
+            return i
+        }
+    }
+    return-1
+}
